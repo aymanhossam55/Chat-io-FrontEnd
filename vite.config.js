7d846cb4a6169abfbf2a809bc02944aaf260d13a
aymanhossam55 <100770611+aymanhossam55@users.noreply.github.com>
2024-12-19 04:59:41 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
+// https://vitejs.dev/config/
 export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: process.env.VITE_SOCKET_URL || "http://localhost:5000",
-        changeOrigin: true,
-      },
-    },
+        target: "http://localhost:5000"
+      }
+    }
   },
-  plugins: [react()],
+  plugins: [react()]
 });
