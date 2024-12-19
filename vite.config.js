19b975848023cc9e5ef3eef9063a5b71c70df2bd
aymanhossam55 <100770611+aymanhossam55@users.noreply.github.com>
2024-12-19 04:02:57 +0200

    make vercel.json

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:5000"
-      }
-    }
+        target: process.env.VITE_SOCKET_URL || "http://localhost:5000",
+        changeOrigin: true,
+      },
+    },
   },
-  plugins: [react()]
+  plugins: [react()],
 });
